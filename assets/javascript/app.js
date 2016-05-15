80945c244a2c31a22cf0f40f55cc6550c87227c6
Kyle Rinehart <Kyle Rinehart>
2016-05-14 23:47:51 -0400

    HTML id's added to most elemets for css and/or JS data manipulation, css started (most items centered and done), JS started, need to figure out proper firebase data storage and manipulation to switch bewteen players

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,48 @@
 //start firebase with 2 objects, a chat div/section, and turn count
 //for enter name, add first log in to first object, set if logic for if first filled, add to second object
+
+//Ensures webpage is loaded before running JS
+$(document).on('ready', function(){
+	//sets url for variable for multiple storage spaces
+  var url = "http://intense-heat-4472.firebaseio.com/"
+  	//sets Firebase memory aside for 2 players
+  var p1 = new Firebase(url);
+  var p2 = new Firebase(url);
+  	//sets Firebase memory for chat window
+  var chat = new Firebase(url);
+  	//sets Firebase memory for turn counter
+  var turn = new Firebase(url);
+  	//inital inputs are blank
+  var name = "";
+  var choice = "";
+  var wins = 0;
+  var losses = 0;
+  var ties = 0;
+  var player1 = true;
+
+  	//when subit name button is clicked
+  $('#input-name').on('click', function(){
+  	//sets player1 data storage
+  	p1.set({
+  		name: name,
+  		choice: choice,
+  		wins: wins,
+  		losses: losses,
+  		ties: ties,
+  	})
+  	//sets player2 data storage
+  	p2.set({
+  		name: name,
+  		choice: choice,
+  		wins: wins,
+  		losses: losses,
+  		ties: ties,
+  	})
+  	//condition modifier to set player2 if player1 present
+  	if(p1.name != ""){
+
+  	}
+
+  })
+
+})
