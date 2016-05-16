d390d275fcdf2bf94bfb3a9230d6826a65464f92
Kyle Rinehart <Kyle Rinehart>
2016-05-15 21:06:25 -0400

    more JS updates, assignment of 2 players now available thanks to @cmckinley, starting work on other gameplay aspects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,13 +5,6 @@
 $(document).on('ready', function(){
 	//sets url for variable for multiple storage spaces
   var url = "http://intense-heat-4472.firebaseio.com/"
-  	//sets Firebase memory aside for 2 players
-  var p1 = new Firebase(url);
-  var p2 = new Firebase(url);
-  	//sets Firebase memory for chat window
-  var chat = new Firebase(url);
-  	//sets Firebase memory for turn counter
-  var turn = new Firebase(url);
   	//inital inputs are blank
   var name = "";
   var choice = "";
@@ -19,47 +12,80 @@ $(document).on('ready', function(){
   var losses = 0;
   var ties = 0;
   var player1 = true;
+  var p1 = '1';
+  var p2 = '2';
+  var players = 'players';
+
   	//when subit name button is clicked
   $('#input-name').on('click', function(){
+  	//sets Firebase storage for game refernce
+ 	var game = new Firebase(url);
   	//takes name from input field
   	name = $('#name').val().trim();
+  	//verifies name capture
   	console.log(name);
-  	//sets player1 data storage
-  	p1.set({
-  		name: name,
-  		choice: choice,
-  		wins: wins,
-  		losses: losses,
-  		ties: ties,
-  	});
+  	//function that only happens 'once' instead of every time value changes
+  	game.once('value', function(snapShot){
+  	  //sets up ability to check if a players object exists
+  	  var exists = snapShot.child(players).exists();
+  	  //sets ability to check if players ojbect has a 'second' player object
+  	  var full = snapShot.child(players).child('2').exists();
+  	  //runs assign player function to set players
+  	  assignPlayer(name, game, exists, full);
+  	})
 
-  	//condition modifier to set player2 if player1 present
-  	if(p1.name != ""){
-	  //sets player2 data storage
-	  p2.set({
-	  	name: name,
-	  	choice: choice,
-	  	wins: wins,
-	  	losses: losses,
-	  	ties: ties,
-	  });
+  	function assignPlayer(name, game, exists, full){
+	  //condition modifier to set player2 if player1 present
+	  if(exists && !full){
+	  	//creates players object in game object
+  		var playersRef = game.child(players);
+  		//creates player2 object in players object
+  		var player2Ref = playersRef.child(p2);
+		//sets player2 data storage
+		player2Ref.set({
+		  name: name,
+		  choice: choice,
+		  wins: wins,
+		  losses: losses,
+		  ties: ties,
+		 });
+		//tests data collection
+	  	console.log(name);
+	  	//sets player 2 name to player 2 div
+	  	$('#player2').html(name);
+	  }
+	  else if (full) {
+	  	alert("Players are set, please try again later");
+	  }
+	  else {
+	  	//creates players object in game object
+  		var playersRef = game.child(players);
+  		//creates player2 object in players object
+  		var player1Ref = playersRef.child(p1); 
+	  	//sets player1 data storage
+	  	player1Ref.set({
+	  		name: name,
+	  		choice: choice,
+	  		wins: wins,
+	  		losses: losses,
+	  		ties: ties,
+	  	});
+	  	//tests data collection
+	  	console.log(name);
+	  	//sets player 1 name to player 1 div
+	  	$('#player1').html(name);
+	  }
   	}
+
+
+
   })
-  //when player1 name is set, return info to player screen
-  p1.on('value', function(data){
-  	console.log(data.val());
-  	console.log(data.val().name)
-  }
-  , function(errorObject){
-  	console.log('Errors handled: '+errorObject.code)
-  });
-  //when player2 name is set, return info to player screen
-  p2.on('value', function(data){
-  	console.log(data.val());
-  	console.log(data.val().name)
+  
+  function chat(message){
 
   }
-  , function(errorObject){
-  	console.log('Errors handled: '+errorObject.code)
-  });
+
+  if (game.child(players).child('1').exists() == true) {
+  	$('#player1').html(game.child(players).child('1').name);
+  }
 })
